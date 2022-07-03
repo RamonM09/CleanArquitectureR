1af510240a22c00e0164615cbbac90f03cce9799
rmagdaleno0v <rmagdaleno0v@gmail.com>
2022-07-03 16:48:03 -0500

    Run The Firts Migratios to DB

---
+++ CA.MigratorDB/Main/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.MigratorDB
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IServiceCollection services = ConfigureServices();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            await serviceProvider.GetService<App>().RunAsync(args);
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true);
+            return builder.Build();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            /* Lectura de opciones del archivo de configuración. */
+            services.Configure<ConnectionStringCollection>(options => config.GetSection($"CollectionConnectionStrings").Bind(options));
+
+            /* Inyectamos la clase 'App' */
+            services.AddSingleton<App>();
+
+            /* Otros servicios de la aplicación de la consola. */
+            //services.AddTransient<IUser, User>();
+
+            return services;
+        }
+    }
+}
